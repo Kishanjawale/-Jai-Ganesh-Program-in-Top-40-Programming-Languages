ec4fa6fd59cd5b6258698ae7cd8c69c7961461f7
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-02-01 00:53:17 +0530

    latest

---
+++ Kotlin_Programming.kt
@@ -0,0 +1,14 @@
+fun main()
+    {
+    println("Jai Ganesh...")
+    }
+
+/*
+Command to create the jar file of our kotlin code;
+kotlinc  Kotlin_Programming.kt -include-runtime -d Kotlin_Programming.jar
+
+
+command to run the jar file created by above command.
+java -jar Kotlin_Programming.jar
+
+*/
